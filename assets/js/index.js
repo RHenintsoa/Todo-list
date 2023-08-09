ed9444d1f700a909a04f95f4b4415a59d975fcb2
RHenintsoa <henintsoarasolofohery@gmail.com>
2023-08-09 22:36:29 +0300

    the add and delete buttons are fonctionnal/ method new Date update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,46 @@
    // Récupérer le jour
-let weekday =['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche'];
-let day = (new Date().getDay());
- for ( day = 0; day < weekday.length ; day++) {
-    let dayd = weekday[day];
-    document.getElementById('day').innerHTML = dayd;
- }
-   // Récupérer la date
-let date = (new Date().getDate());
-document.getElementById('date').innerHTML = date;
-   // Récupérer le mois
-let months =['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
-let monthIndex  = (new Date().getMonth());
-let month = months[monthIndex];
-document.getElementById('month').innerHTML= month;
+const date = new Date();
+const display = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+let moment =(date.toLocaleString('fr-IN', display));
+document.getElementById('date').innerHTML = moment;
 
- // Récupérer l'année
-let year = (new Date().getFullYear());
-document.getElementById('year').innerHTML = year;
-// Récupérer les heures et les minutes
-// let hour = (new Date().getHours());
-// let minute = (new Date().getMinutes());
-// let second = (new Date().getSeconds());
 
-// Affichage to-do qu'on vient d'ajouter avec le déclencheur ajouter
+// Affichage 
 let btnAdd = document.getElementById('btn-add');
 let todoList = document.getElementById('todo-list');
 btnAdd.addEventListener('click', function(){
+   // Add
    let myTodo = document.getElementById('input-todo').value;
    let myList = document.createElement('tr');
     myList.innerHTML = `<td class="sticky" id="my-todo">`+myTodo+` </td>
                         <td><button class=" btn btn-edit"><span class="bi bi-stickies"></span></button></td>
                         <td><button class="btn btn-delete"> <span class="bi bi-archive"></span></button></td>`
 todoList.appendChild(myList);
+document.getElementById('input-todo').value ="";
+   // Delete
+let deleteButtons = document.getElementsByClassName('btn btn-delete');
+for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener('click', function() {
+        this.closest('tr').remove();
+    });
+}
+   // Edit 
+  let editButtons = document.getElementsByClassName('btn btn-edit');
+  for (let i = 0; i< editButtons.length; i++){
+   editButtons[i].onclick = function(){         
+      document.getElementById('line-add').style.display="none";
+      document.getElementById('line-edit').style.display="block";     
+      console.log(this.closest(myTodo[i]));
+   }
+  }
+
 });
+
+
+
 
